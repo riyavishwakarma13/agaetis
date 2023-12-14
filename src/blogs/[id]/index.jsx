ac1f8475ee8c793c577061ba787737d7bac26827
Riya Vishwakarma <riyavishwakarma.alf@gmail.com>
2023-12-14 22:07:44 +0530

    aside sections

--- src/blogs/[id]/index.jsx
+++ src/blogs/[id]/index.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const BlogDesc = () => {
   return (
     <main className="px-1 space-y-2 py-6">
+      {/* title */}
       <div className="bg-[#343A40] rounded text-white grid lg:grid-cols-2 mx-1 place-items-center py-14 px-10">
         <div className="space-y-5">
           <h1 className="text-2xl md:text-5xl font-serif italic font-bold">
@@ -17,6 +18,7 @@ const BlogDesc = () => {
         </div>
         <div></div>
       </div>
+      {/* article */}
       <div className="flex py-6 flex-wrap justify-center mx-1 items-center gap-10">
         <article className="grid md:grid-cols-3 rounded gap-5 border border-gray-500 max-w-[507px] place-items-center">
           <div className="col-span-2 px-4 space-y-3 py-4">
@@ -52,6 +54,45 @@ const BlogDesc = () => {
             </div>
           </div>
         </article>
+      </div>
+      <div className="flex flex-col lg:flex-row justify-between mx-1 py-6 items-start">
+        <section>ffdffdfdf</section>
+        {/* aside section */}
+        <aside className="w-1/3 lg:block hidden">
+          <div className="bg-gray-100 mb-5 rounded p-4">
+            <h1 className="font-serif font-bold text-xl italic">About</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              saepe eligendi incidunt recusandae voluptates dolorem fuga culpa
+              non odit, voluptatum, ipsam facilis.
+            </p>
+          </div>
+          <div className="py-5 space-y-2">
+            <h2 className="font-serif font-bold text-xl italic">Archives</h2>
+            <ul className="list-none text-blue-500 font-medium">
+              <li>March 2014</li>
+              <li>February 2014</li>
+              <li>January 2014</li>
+              <li>December 2013</li>
+              <li>November 2013</li>
+              <li>October 2013</li>
+              <li>September 2013</li>
+              <li>August 2013</li>
+              <li>July 2013</li>
+              <li>June 2013</li>
+              <li>May 2013</li>
+              <li>April 2013</li>
+            </ul>
+          </div>
+          <div className="py-5 space-y-2">
+            <h2 className="font-serif font-bold text-xl italic">Elsewhere</h2>
+            <ul className="list-none text-blue-500 font-medium">
+              <li>GitHub</li>
+              <li>Twitter</li>
+              <li>Facebook</li>
+            </ul>
+          </div>
+        </aside>
       </div>
     </main>
   );
